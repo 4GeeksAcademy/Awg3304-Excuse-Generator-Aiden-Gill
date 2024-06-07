e791e826e77cf2070c146de2c4d2a0281df3a0a1
Awg3304 <166061446+Awg3304@users.noreply.github.com>
2024-06-07 22:24:24 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = document.getElementById("excuse");
+  let who = ["Hulk", "Iron Man", "The Thing", "Spider-Man"];
+  let action = ["demolished", "smashed", "broke", "disintegrated"];
+  let what = ["my car", "my computer", "my house"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function getRandomInt(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+  excuse.innerHTML =
+    who[getRandomInt(who)] +
+    " " +
+    action[getRandomInt(action)] +
+    " " +
+    what[getRandomInt(what)] +
+    " " +
+    when[getRandomInt(when)];
 };
